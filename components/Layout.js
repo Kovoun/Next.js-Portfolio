7a224420ee78468009c5ada5274013dc75beba73
Tomáš Kovařík <codingkovoun@gmail.com>
2022-12-22 19:03:46 +0100

    meta tagy

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Footer from "./Footer"
 import Navigation from "./NavBar"
 
 const Layout = ({children}) => {
     return (
         <>
+            <Head>
+                <meta name="description" content="I deal with coding websites to your liking with all the functionality" />
+                <meta name="robots" content="index,follow" />
+                <meta charset="utf-8" />
+                <meta property="og:title" content="Tomáš Kovařík" />
+            </Head>
             <Navigation />
             <div>
                 {children}
