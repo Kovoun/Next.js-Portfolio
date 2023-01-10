825885fa3bfa4343782d0d417ad547b6c8ce4910
Tomáš Kovařík <codingkovoun@gmail.com>
2023-01-10 19:00:40 +0100

    Automatic updating Year, captions for pictures

--- components/Footer.js
+++ components/Footer.js
@@ -3,25 +3,27 @@ import email from "../public/e-mail.svg"
 import slack from "../public/slack.svg"
 import Image from "next/image"
 import Link from "next/link";
+import { useState } from "react";
 
-const automaticYear = () => {
-  document.write(new Date().getFullYear())
-}
 
 const Footer = () => {
+  
+  const [ currentYear, setCurrentYear ] = useState(new Date().getFullYear());
+
+
   return (
     <>
       <footer className="footer">
         <div className="row">
           <div className="footer-contacts">
             <Link href="mailto:codingkovoun@gmail.com">
-              <Image src={email} alt="somezihndijg" />
+              <Image src={email} alt="e-mail" />
             </Link>
             <Link href="https://github.com/Kovoun">
-              <Image src={github} alt="fghjfhfh" />
+              <Image src={github} alt="github" />
             </Link>
           </div>
-          <p className="footer-paragraph">©Tomáš Kovařík {automaticYear()}</p>
+          <p className="footer-paragraph">©Tomáš Kovařík {currentYear}</p>
         </div>
       </footer>
     </>
